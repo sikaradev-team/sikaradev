bf41ed595a3c64fb7d0528d558bee7440c3fa213
bensonirah <bensonirah@gmail.com>
2022-04-09 12:37:54 +0300

    add custom favicons

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { useContext } from "react";
 import Footer from "./footer";
 import Navbar from "./navbar";
 
@@ -9,8 +8,26 @@ export default function Layout({ children, home }) {
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>SakraDev Project</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Home|sikara-dev</title>
+        
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <header className="bg-team-1">
         <Navbar />
